a4bb2af4c67ae5452a908a384173d12083ecc573
Ahmad AlKofahi <AKofahi@outlook.com>
2022-03-19 23:38:35 +0300

    Color on Description Error Handling Test on Other PC Error Fixed after use up to 15/2/2022

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -34,15 +34,31 @@
             this.button3 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Confirmation = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comp = new System.Windows.Forms.Label();
+            this.currentlyOn = new System.Windows.Forms.Label();
+            this.total = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(69, 15);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(143, 95);
             this.button1.TabIndex = 1;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
@@ -50,30 +66,32 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 90);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(69, 116);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(143, 95);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "التعبئة الالية";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(742, 12);
+            this.textBox1.Location = new System.Drawing.Point(299, 12);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(116, 333);
+            this.textBox1.Size = new System.Drawing.Size(116, 340);
             this.textBox1.TabIndex = 4;
             this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(635, 12);
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(299, 400);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(116, 38);
             this.button3.TabIndex = 5;
-            this.button3.Text = "pop_Query";
+            this.button3.Text = "انقل للبرنامج";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
@@ -81,7 +99,7 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(478, 12);
+            this.listBox1.Location = new System.Drawing.Point(421, 12);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(120, 340);
             this.listBox1.TabIndex = 6;
@@ -90,39 +108,177 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(383, 19);
+            this.label1.Location = new System.Drawing.Point(113, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(0, 16);
             this.label1.TabIndex = 7;
-            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(296, 355);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 32);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "الارقام الي بدك \r\n تعملهم حطهم هون";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(418, 355);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 32);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "الارقام الي صارت\r\n جوا البرنامج";
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(11, 153);
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(422, 400);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 8;
-            this.button4.Text = "button4";
+            this.button4.Size = new System.Drawing.Size(119, 38);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "فضي البرنامج";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(675, 355);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 48);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "صندوق النتائج\r\nاخضر صحيح\r\nاحمر في خطئ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Confirmation});
+            this.dataGridView1.Location = new System.Drawing.Point(548, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(517, 340);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.MinimumWidth = 6;
+            this.ID.Name = "ID";
+            this.ID.Width = 125;
+            // 
+            // Confirmation
+            // 
+            this.Confirmation.HeaderText = "Confirmation";
+            this.Confirmation.MinimumWidth = 6;
+            this.Confirmation.Name = "Confirmation";
+            this.Confirmation.Width = 125;
+            // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(38, 196);
+            this.button5.Location = new System.Drawing.Point(678, 415);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 9;
-            this.button5.Text = "button5";
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Copy";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.total);
+            this.groupBox1.Controls.Add(this.currentlyOn);
+            this.groupBox1.Controls.Add(this.comp);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(12, 218);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(278, 220);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 16);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Last Completed:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 77);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 16);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Currently on:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(6, 137);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 16);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Total:";
+            // 
+            // comp
+            // 
+            this.comp.AutoSize = true;
+            this.comp.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comp.Location = new System.Drawing.Point(6, 44);
+            this.comp.Name = "comp";
+            this.comp.Size = new System.Drawing.Size(12, 16);
+            this.comp.TabIndex = 3;
+            this.comp.Text = "-";
+            // 
+            // currentlyOn
+            // 
+            this.currentlyOn.AutoSize = true;
+            this.currentlyOn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentlyOn.Location = new System.Drawing.Point(6, 102);
+            this.currentlyOn.Name = "currentlyOn";
+            this.currentlyOn.Size = new System.Drawing.Size(12, 16);
+            this.currentlyOn.TabIndex = 4;
+            this.currentlyOn.Text = "-";
+            // 
+            // total
+            // 
+            this.total.AutoSize = true;
+            this.total.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.total.Location = new System.Drawing.Point(6, 169);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(30, 16);
+            this.total.TabIndex = 16;
+            this.total.Text = "- / -";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(870, 450);
+            this.ClientSize = new System.Drawing.Size(1075, 450);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button3);
@@ -130,9 +286,12 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "PopEm";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -145,8 +304,21 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Confirmation;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label total;
+        private System.Windows.Forms.Label currentlyOn;
+        private System.Windows.Forms.Label comp;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -6,22 +6,23 @@ using System.Windows.Forms;
 using System.Threading;
 using System.Timers;
 using System.Threading.Tasks;
+using System.Data;
+using System.Drawing;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-
-        IWebDriver driver = new ChromeDriver();
-        WebDriverWait wait;
+        readonly IWebDriver driver = new ChromeDriver();
+        readonly WebDriverWait wait;
+        int counter = 0;
 
 
         string[] lines;
         public Form1()
         {
             InitializeComponent();
-            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(20);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+     
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
             
         }
@@ -43,7 +44,7 @@ namespace WindowsFormsApp1
             }
             catch (WebDriverTimeoutException)
             {
-                label1.Text = "hey hey";
+                
                 driver.FindElement(By.TagName("body")).SendKeys("Keys.ESCAPE");
             }
 
@@ -51,27 +52,79 @@ namespace WindowsFormsApp1
 
         private void Button2_Click(object sender, EventArgs e)
         {
-
+            int i = 0;
+            DataTable table = new DataTable();
+            table.Columns.Add("ID", typeof(int));
+            table.Columns.Add("Confirmation", typeof(int));
             foreach(string line in lines)
             {
-                try { driver.Navigate().GoToUrl("https://eservices.moh.gov.sa/CoronaVaccineRegistrationMA/Home");
+                currentlyOn.Text = line;
+                IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+                driver.Navigate().GoToUrl("https://eservices.moh.gov.sa/CoronaVaccineRegistrationMA/Home");
                     Task.Delay(2000);
                     IWebElement talabat = driver.FindElement(By.XPath("/html/body/div[2]/div/aside/div[2]/nav/ul/li/ul/li[2]/a/span"));
-                    talabat.Click();
-                    Task.Delay(1000);
+                executor.ExecuteScript("arguments[0].click();", talabat);
+                Task.Delay(1000);
                     IWebElement daleh = driver.FindElement(By.XPath("/html/body/div[2]/div/div/main/form/div/div[2]/div/div/div/div[2]/div[1]/div[1]/input"));
                     daleh.SendKeys(line);
                     IWebElement search = driver.FindElement(By.XPath("/html/body/div[2]/div/div/main/form/div/div[2]/div/div/div/div[2]/div[6]/button[1]"));
-                    search.Click();
+                executor.ExecuteScript("arguments[0].click();", search);
+
                     IWebElement khaiart = driver.FindElement(By.XPath("/html/body/div[2]/div/div/main/form/div/div[2]/div/div/div/div[2]/table[2]/tbody/tr/th[2]"));
                     driver.Navigate().GoToUrl("https://eservices.moh.gov.sa/CoronaVaccineRegistrationMA/VaccineRequest/Action/" + khaiart.GetAttribute("innerHTML"));
-                    
-                  
+
+                    Task.Delay(2000).Wait();
+                    IWebElement ejra2 = driver.FindElement(By.CssSelector("#select2-Model_Status-container"));
+                    ((IJavaScriptExecutor)driver).ExecuteScript(script: "arguments[0].scrollIntoView(true)", ejra2);
+                    Task.Delay(1000).Wait();
+                    ejra2.Click();
 
 
+                    Task.Delay(1000).Wait();
+                driver.FindElement(By.XPath("/html/body/span/span/span[2]/ul/li[2]")).Click();
                
+               
+                    try 
+                    {
+                        IWebElement submit = driver.FindElement(By.XPath("/html/body/div[2]/div/div/main/form/div/div[2]/div/div[10]/button"));
+                        ((IJavaScriptExecutor)driver).ExecuteScript(script: "arguments[0].scrollIntoView(true)", submit);
+                        submit.Click();
+
+                        Task.Delay(5000).Wait();
+                        IWebElement NVR = driver.FindElement(By.XPath("/html/body/div[2]/div/div/main/form/div/div[2]/div/div[10]/button[2]"));
+                        executor.ExecuteScript("arguments[0].click();", NVR);
+
+                        Task.Delay(4000).Wait();
+                        IWebElement conf = driver.FindElement(By.XPath("/html/body/div[2]/div/div/main/div[3]/div/div/div[2]/div/h4"));
+
+
+                        if (conf.GetAttribute("innerHTML").Contains("تم إرسال جميع الجرعات"))
+                        {
+                            dataGridView1.Rows.Add(line, "تم إرسال جميع الجرعات");
+                            dataGridView1.Rows[i].Cells[1].Style.BackColor = Color.Green;
+
+                        }
+                        else if (conf.GetAttribute("innerHTML").Contains("تم إرسال جرعه واحدة من هذا الطلب"))
+                        {
+                            dataGridView1.Rows.Add(line, "تم إرسال جرعه واحدة من هذا الطلب");
+                            dataGridView1.Rows[i].Cells[1].Style.BackColor = Color.Yellow;
+                        }
+                        else
+                        {
+                            dataGridView1.Rows.Add(line, "حدث خطأ أثناء ارسال الجرعات");
+                            dataGridView1.Rows[i].Cells[1].Style.BackColor = Color.Red;
+                        }
+                    }
+                
+                catch (NoSuchElementException)
+                {
+                    dataGridView1.Rows.Add(line, "معمول مسبقا");
+                    dataGridView1.Rows[i].Cells[1].Style.BackColor = Color.White;
                 }
-                catch (WebDriverTimeoutException) { }
+                
+                i++;
+                total.Text = i + " / " + counter;
+                comp.Text = line;
             }
             
           
@@ -81,7 +134,10 @@ namespace WindowsFormsApp1
         {
             lines = textBox1.Text.Split('\n');
             foreach (string line in lines)
+            {
                 listBox1.Items.Add(line);
+                counter++;
+            }
 
         }
 
@@ -101,26 +157,28 @@ namespace WindowsFormsApp1
 
         }
 
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
         private void button4_Click(object sender, EventArgs e)
         {
-           IWebElement ejra2 = driver.FindElement(By.CssSelector("#select2-Model_Status-container"));
-            ((IJavaScriptExecutor)driver).ExecuteScript(script: "arguments[0].scrollIntoView(true)", ejra2);
-            ejra2.Click();
-            Task.Delay(500);
-            driver.FindElement(By.XPath("/html/body/span/span/span[2]/ul/li[2]")).Click();
-            IWebElement submit = driver.FindElement(By.XPath("/html/body/div[2]/div/div/main/form/div/div[2]/div/div[9]/button"));
-            ((IJavaScriptExecutor)driver).ExecuteScript(script: "arguments[0].scrollIntoView(true)", submit);
-            submit.Click();
-            
-           
-
+            listBox1.Items.Clear();
         }
-
+        public void CopyToClipboardWithHeaders(DataGridView _dgv)
+        {
+            //Copy to clipboard
+            _dgv.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+            DataObject dataObj = _dgv.GetClipboardContent();
+            if (dataObj != null)
+                Clipboard.SetDataObject(dataObj);
+        }
         private void button5_Click(object sender, EventArgs e)
         {
-            Task.Delay(2000).Wait();
-            IWebElement NVR = driver.FindElement(By.XPath("/html/body/div[2]/div/div/main/form/div/div[2]/div/div[9]/button[2]"));
-            NVR.Click();
+            CopyToClipboardWithHeaders(dataGridView1);
         }
+
+        
     }
 }
